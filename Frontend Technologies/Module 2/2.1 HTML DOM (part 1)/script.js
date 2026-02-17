6f79ce769eb42bfc09fa3b87eb8237416b346749
Sander Karlsen <sander.k606@gmail.com>
2026-02-17 09:56:51 +0100

    Update script.js

--- Frontend Technologies/Module 2/2.1 HTML DOM (part 1)/script.js
+++ Frontend Technologies/Module 2/2.1 HTML DOM (part 1)/script.js
@@ -1,20 +1,24 @@
-setInterval(() =>{
-    if(Math.random() > 0.5){
-        document.getElementById("para1").style.fontSize = "50px";
-    } else {
-        document.getElementById("para1").style.fontSize = "10px";
+function randomColor() {
+    var random = Math.random();
+
+    if(random < 1/3){
+        return "yellow";
     }
+
+    else if(random < 2/3) {
+        return "magneta";
+    }
+    return "cyan";
+}
+
+setInterval(() => {
+    currentFont = window.getComputedStyle(document.getElementById("para1")),
+    null.getPropertyValue(font-size);
+
+    parsedFont = parseFloat(currentFont.slice(0, -2));
+    document.getElementById("para1").stylle.fontSize = (Math.random() > 0.5 ? parsedFont * 2 : parsedFont / 2).toString() + "px";
 }, 5000)
+setInterval(() => document.getElementById("para2").innerHTML = "Date : " + Date(), 1000)
 
-setInterval(() => document.getElementById("para2").innerHTML = "Date: " + Date(), 10000)
-
-setInterval(() =>{
-    const random = Math.random();
-    if(random < 0.33){
-        document.getElementById("para3").style.backgroundColor = "Yellow";
-    } else if (random < 0.66) {
-        document.getElementById("para3").style.backgroundColor = "Magenta";
-    } else {
-        document.getElementById("para3").style.backgroundColor = "Cyan";
-    }
-}, 3000)
+setInterval(() =>
+document)
